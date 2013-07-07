4363bd7d375453713746574f1417194d4a3272e5
jumpinjan <janice.gluck@excella.com>
2013-07-07 08:18:47 -0400

    updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-FirstApp::Application.config.secret_key_base = 'f660444e3713ff99a0cbc285c1bad63bf85468fc6130753de22414a60743a9263b5e6a9739e19d9f5a5cea1c0671b4afa01767bb1e525ba56ef1753ca0c517b1'
+FirstApp::Application.config.secret_token = 'f8f2da0fbbcc34d87a02dac71136b9a99339aabd2201d5ded03bc30fa9a892d3d27b493a6e13f439bd760f90fe3232d9ca3a6b631245a37e3cfcbb1b5a828f7a'
---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '452e0edd7c56b2dc30ce02654818a759d40703be7df37b3cf6758d793d0b6fe2ec68d6e60b5205ab4e547480f6eb6a16716a83e905746ddfa7fb7ead54b60f24'
